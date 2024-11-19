8fefe6cdac66eec4b2036b28ad38ed73442dff33
Faznug_Space <153061199+FaishalFaiz@users.noreply.github.com>
2024-11-19 09:25:00 +0700

    new feature : iuser location and shalat schedule API

--- lib/presentation/screen/jadwal_sholat_screen.dart
+++ lib/presentation/screen/jadwal_sholat_screen.dart
@@ -1,9 +1,62 @@
+import 'dart:convert';
+
 import 'package:bitaqwa_app/presentation/widgets/time.dart';
 import 'package:bitaqwa_app/utils/color.dart';
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import 'package:permission_handler/permission_handler.dart';
 
-class JadwalSholatScreen extends StatelessWidget {
+class JadwalSholatScreen extends StatefulWidget {
   const JadwalSholatScreen({super.key});
+
+  @override
+  State<JadwalSholatScreen> createState() => _JadwalSholatScreenState();
+}
+
+class _JadwalSholatScreenState extends State<JadwalSholatScreen> {
+  // Get API data Function
+  Future<List<dynamic>> getJadwalSholat() async {
+    final url =
+        "https://cdn.statically.io/gh/lakuapik/jadwalsholatorg/master/adzan/karanganyar/2024/11.json";
+    final response = await http.get(Uri.parse(url));
+    //2 pengkondisian jika data berhahasil =di dapat dan tidak berhasil
+    if (response.statusCode == 200) {
+      print(response.body);
+      return jsonDecode(response.body);
+    } else {
+      throw Exception("Failed to load data");
+    }
+  }
+
+  // function for getting user location
+  Future<void> getLocation() async {
+    // asking user location permission
+    var status = await Permission.location.request();
+    if (status.isGranted) {
+      try {
+        // getting user coordinate point
+        Position position = await Geolocator.getCurrentPosition();
+        // get location name
+        List<Placemark> placemark = await placemarkFromCoordinates(
+            position.latitude, position.longitude);
+        Placemark place = placemark.first;
+        print(place.subLocality);
+      } catch (e) {
+        print("Terjadi Error : $e");
+      }
+    } else {
+      print("Akses tidak diizinkan");
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getJadwalSholat();
+    getLocation();
+  }
 
   @override
   Widget build(BuildContext context) {
